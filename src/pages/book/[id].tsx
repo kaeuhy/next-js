2c50c28d89cf5ca233c37da0310d53a09593e508
kaeuhy <kaeuhy_0315@naver.com>
2026-07-06 20:35:46 +0900

    페이지별 SSG 적용

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,4 +1,7 @@
 import style from "./[id].module.css";
+import {GetServerSidePropsContext, InferGetServerSidePropsType} from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchOneBook from "@/lib/fetch-one-book";
 
 const mockData = {
   id: 1,
@@ -12,7 +15,19 @@ const mockData = {
     "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg",
 };
 
-export default function Page() {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+    const id = context.params!.id;
+    const book = await fetchOneBook(Number(id));
+    return {
+        props: {
+            book
+        }
+    }
+}
+
+export default function Page({book}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    if (!book) return "문제가 발생햇습니다 다시 시도하세요";
+
   const {
     id,
     title,
@@ -21,7 +36,7 @@ export default function Page() {
     author,
     publisher,
     coverImgUrl,
-  } = mockData;
+  } = book;
 
   return (
     <div className={style.container}>
