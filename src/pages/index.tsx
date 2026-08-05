8dd16d7f3ce1da73e5b6f2feed0429bddf4cd090
kaeuhy <kaeuhy_0315@naver.com>
2026-08-06 01:11:42 +0900

    SEO 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
 // CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 import BookItem from "@/components/book-item";
 import fetchBooks from "@/lib/fetch-books";
 import {InferGetStaticPropsType} from "next";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
     // const allBooks = await fetchBooks();
     // const recoBooks = await fetchRandomBooks();
-
-    console.log("인덱스 페이지");
 
     const [allBooks, recoBooks] = await Promise.all([fetchBooks(), fetchRandomBooks()])
 
@@ -26,24 +25,32 @@ export const getStaticProps = async () => {
 };
 
 export default function Home({allBooks, recoBooks,}: InferGetStaticPropsType<typeof getStaticProps>) {
-  return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {recoBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
-  );
+    return (
+        <>
+            <Head>
+                <title>한입북스</title>
+                <meta property="og:image" content="/thumbnail.png"/>
+                <meta property="og:title" content="한입북스"/>
+                <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요"/>
+            </Head>
+            <div className={style.container}>
+                <section>
+                    <h3>지금 추천하는 도서</h3>
+                    {recoBooks.map((book) => (
+                        <BookItem key={book.id} {...book} />
+                    ))}
+                </section>
+                <section>
+                    <h3>등록된 모든 도서</h3>
+                    {allBooks.map((book) => (
+                        <BookItem key={book.id} {...book} />
+                    ))}
+                </section>
+            </div>
+        </>
+    );
 }
 
 Home.getLayout = (page: ReactNode) => {
-  return <SearchableLayout>{page}</SearchableLayout>;
+    return <SearchableLayout>{page}</SearchableLayout>;
 };
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -5,6 +5,7 @@ import {GetServerSidePropsContext, GetStaticPropsContext, InferGetServerSideProp
 import fetchBooks from "@/lib/fetch-books";
 import {useRouter} from "next/router";
 import {BookData} from "@/types";
+import Head from "next/head";
 
 // export const getStaticSideProps = async(context: GetStaticPropsContext) => {
 //   const q = context.query.q;
@@ -37,6 +38,12 @@ export default function Page() {
 
   return (
     <div>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png"/>
+        <meta property="og:title" content="한입북스"/>
+        <meta property="og:description" content="한입 북스 - 검색결과"/>
+      </Head>
       {books.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
